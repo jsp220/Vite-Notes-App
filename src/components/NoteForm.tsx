e0836ac2f1b088f7bc9c19ff5aa1468c194bf61f
Joon Park <jsp220@gmail.com>
2023-01-29 22:48:06 -0800

    useRefs and useState to handle data when submitted

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,19 +1,31 @@
-import { useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
+import { NoteData, Tag } from "../App";
 
-export function NoteForm() {
-    const titleRef = useRef(null);
-    const markdownRef = useRef(null);
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+}
 
-    // function handleSubmit() {
-    //     e.preventDefault();
-    // }
+export function NoteForm({ onSubmit }: NoteFormProps) {
+    const titleRef = useRef<HTMLInputElement> (null)
+    const bodyRef = useRef<HTMLTextAreaElement>(null)
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault()
+
+        onSubmit({
+            title: titleRef.current!.value,
+            body: bodyRef.current!.value,
+            tags: []
+        })
+    }
 
     return (
         // <Form onSubmit={handleSubmit}>
-        <Form>
+        <Form onSubmit={handleSubmit }>
             <Stack gap={4}>
                 <Row>
                     <Col>
@@ -32,11 +44,13 @@ export function NoteForm() {
                 <Row>
                     <Form.Group controlId="markdown">
                         <Form.Label>Body</Form.Label>
-                        <Form.Control required as="textarea" ref={markdownRef} rows={15} />
+                        <Form.Control required as="textarea" ref={bodyRef} rows={15} />
                     </Form.Group>
                 </Row>
                 <Stack direction="horizontal" gap={2} className="justify-content-end">
-                    <Button type="submit" variant="primary">Save</Button>
+                    <Button type="submit" variant="primary">
+                        Save
+                    </Button>
                     <Link to="..">
                         <Button type="button" variant="outline-secondary">Cancel</Button>
                     </Link>
